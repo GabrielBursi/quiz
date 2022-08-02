39541a67d52bf9fc57b1e282ff5b4c004cbebd82
GabrielBursi <gabriellbursii@gmail.com>
2022-08-02 18:20:51 -0300

    funçao onclick para aparecer a opçao responder

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let btn = document.querySelectorAll(".alternativa-input")
 let res = document.querySelector(".btn-container")
 
-var perguntas = ["Qual a linguagem de estilo?","Qual a linguagem de marcação?","Qual dessas linguagens manipula o DOM?"]
+var perguntas = [
+  "Qual a linguagem de estilo?",
+  "Qual a linguagem de marcação?",
+  "Qual dessas linguagens manipula o DOM?"
+]
 var respostas = [{
   opcao1:"HTML",
   opcao2:"CSS",
@@ -17,9 +21,12 @@ var respostas = [{
   opcao2:"HTML",
   opcao3:"CSS"
 }]
-if(btn[0].checked || btn[1].checked || btn[2].checked){
+function alternativa(){
+  if(btn[0].checked || btn[1].checked || btn[2].checked){
   res.innerHTML = "<button id='btn-enviar' onclick='responder()'>Responder</button>"
 }
+}
+
 //if(!btn[0].checked || !btn[1].checked || !btn[2].checked){
 //  res.innerHTML = ""
 //}
